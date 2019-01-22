22418eecaf45a322a8e56a6389062dc4419a882a
zxf <iecaser@163.com>
2019-01-22 11:40:33 +0800

    add sql

---
+++ learn/db/2_zxf.sql
@@ -0,0 +1,37 @@
+﻿select
+	c.user_log_acct,
+	c.batch_id,
+	sum(
+		case c.cps_state_cd
+			when 2
+			then 1
+			else 0
+		end) as giveup_num
+from
+	(
+		select
+			a.*
+		from
+			(
+				select distinct batch_id from tmp.zxf_user_use_batch
+			)
+			b
+		join
+			(
+				select
+					user_log_acct,
+					batch_id,
+					cps_state_cd
+				from
+					gdm.gdm_m07_cps_basic_info
+				where
+					dt = '2018-12-01'
+			)
+			a
+		on
+			a.batch_id = b.batch_id
+	)
+	c
+group by
+	c.user_log_acct,
+	c.batch_id
---
+++ learn/db/xy.sql
@@ -0,0 +1,3 @@
+﻿SELECT  X.user,X.click,y.cart,X.shop_id FROM   x LEFT JOIN y ON x.user= y.user and X.shop_id=Y.shop_id
+UNION 
+SELECT  Y.user,x.click,Y.cart,y.shop_id FROM   x right JOIN y ON x.user= y.user and X.shop_id=Y.shop_id
