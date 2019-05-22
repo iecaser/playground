75ee1a7908a089d4bd3ae3b36468b92db49d3263
zxf <iecaser@163.com>
2019-05-22 22:45:29 +0800

    let xmodmap nohup

--- config/config.sh
+++ config/config.sh
@@ -1,5 +1,5 @@
-xmodmap ~/workspace/playground/config/keyboardmap/CapsLockSuper.xmodmap
-xcape -e 'Mode_switch=Escape'
-sudo sslocal -c /etc/shadowsocks/config.json -d start
+nohup xmodmap ~/workspace/playground/config/keyboardmap/CapsLockSuper.xmodmap > /dev/null 2>&1 &
+nohup xcape -e 'Mode_switch=Escape' > /dev/null 2>&1 &
 nohup ~/cerebro/cerebro-0.3.1-x86_64.AppImage > /dev/null 2>&1 &
 nohup guake > /dev/null 2>&1 &
+sudo sslocal -c /etc/shadowsocks/config.json -d start
