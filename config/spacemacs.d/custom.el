83eb019c1a5da539c3a4aad20d74e4434e7cf03c
zxf <iecaser@163.com>
2019-05-25 02:57:51 +0800

    for debug

--- config/spacemacs.d/custom.el
+++ config/spacemacs.d/custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(bibtex-completion-bibliography (quote ("~/workspace/playground/learn/latex/bib.bib")))
+ '(bibtex-completion-bibliography '("~/workspace/playground/learn/latex/bib.bib"))
  '(display-time-24hr-format t)
  '(display-time-default-load-average nil)
  '(display-time-format "%H:%M:%S")
@@ -14,12 +14,29 @@
  '(global-display-line-numbers-mode t)
  '(global-flycheck-mode t)
  '(multi-term-program "/usr/bin/zsh")
+ '(nyan-bar-length 16)
+ '(nyan-mode t)
  '(org-ref-bibliography-notes "~/papers/notes.org")
- '(org-ref-default-bibliography (quote ("~/workspace/playground/learn/latex/bib.bib")))
+ '(org-ref-default-bibliography '("~/workspace/playground/learn/latex/bib.bib"))
  '(org-ref-pdf-directory "~/papers/")
  '(package-selected-packages
-   (quote
-    (engine-mode dockerfile-mode docker json-mode docker-tramp json-snatcher json-reformat packed package-lint xterm-color shell-pop multi-term eshell-z eshell-prompt-extras esh-help color-theme-sanityinc-tomorrow yasnippet-snippets ws-butler writeroom-mode winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package unfill treemacs-projectile treemacs-evil toc-org symon symbol-overlay string-inflection spaceline-all-the-icons smeargle restart-emacs rainbow-mode rainbow-identifiers rainbow-delimiters pytest pyenv-mode py-isort py-autopep8 popwin pippel pipenv pip-requirements persp-mode pcre2el password-generator paradox overseer orgit org-ref org-projectile org-present org-pomodoro org-mime org-download org-cliplink org-bullets org-brain open-junk-file nameless mwim move-text mmm-mode material-theme markdown-toc magit-svn magit-gitflow macrostep lorem-ipsum live-py-mode link-hint indent-guide importmagic hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-xref helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-org-rifle helm-mode-manager helm-make helm-gitignore helm-git-grep helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot gitignore-templates gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md fuzzy font-lock+ flyspell-correct-helm flycheck-pos-tip flycheck-package flx-ido fill-column-indicator fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-textobj-line evil-surround evil-org evil-numbers evil-nerd-commenter evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu elisp-slime-nav editorconfig dumb-jump dotenv-mode doom-modeline diminish diff-hl devdocs define-word cython-mode csv-mode counsel-projectile company-statistics company-auctex company-anaconda column-enforce-mode color-identifiers-mode clean-aindent-mode centered-cursor-mode browse-at-remote blacken auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent ace-link ace-jump-helm-line ac-ispell)))
+   '(doom-themes parrot ghub treepy graphql engine-mode dockerfile-mode docker json-mode docker-tramp json-snatcher json-reformat packed package-lint xterm-color shell-pop multi-term eshell-z eshell-prompt-extras esh-help color-theme-sanityinc-tomorrow yasnippet-snippets ws-butler writeroom-mode winum which-key volatile-highlights vi-tilde-fringe uuidgen use-package unfill treemacs-projectile treemacs-evil toc-org symon symbol-overlay string-inflection spaceline-all-the-icons smeargle restart-emacs rainbow-mode rainbow-identifiers rainbow-delimiters pytest pyenv-mode py-isort py-autopep8 popwin pippel pipenv pip-requirements persp-mode pcre2el password-generator paradox overseer orgit org-ref org-projectile org-present org-pomodoro org-mime org-download org-cliplink org-bullets org-brain open-junk-file nameless mwim move-text mmm-mode material-theme markdown-toc magit-svn magit-gitflow macrostep lorem-ipsum live-py-mode link-hint indent-guide importmagic hungry-delete hl-todo highlight-parentheses highlight-numbers highlight-indentation helm-xref helm-themes helm-swoop helm-pydoc helm-purpose helm-projectile helm-org-rifle helm-mode-manager helm-make helm-gitignore helm-git-grep helm-flx helm-descbinds helm-company helm-c-yasnippet helm-ag google-translate golden-ratio gnuplot gitignore-templates gitconfig-mode gitattributes-mode git-timemachine git-messenger git-link git-gutter-fringe git-gutter-fringe+ gh-md fuzzy font-lock+ flyspell-correct-helm flycheck-pos-tip flycheck-package flx-ido fill-column-indicator fancy-battery eyebrowse expand-region evil-visualstar evil-visual-mark-mode evil-unimpaired evil-tutor evil-textobj-line evil-surround evil-org evil-numbers evil-nerd-commenter evil-matchit evil-magit evil-lisp-state evil-lion evil-indent-plus evil-iedit-state evil-goggles evil-exchange evil-ediff evil-cleverparens evil-args evil-anzu eval-sexp-fu elisp-slime-nav editorconfig dumb-jump dotenv-mode doom-modeline diminish diff-hl devdocs define-word cython-mode csv-mode counsel-projectile company-statistics company-auctex company-anaconda column-enforce-mode color-identifiers-mode clean-aindent-mode centered-cursor-mode browse-at-remote blacken auto-yasnippet auto-highlight-symbol auto-dictionary auto-compile aggressive-indent ace-link ace-jump-helm-line ac-ispell))
+ '(parrot-mode t nil (parrot))
+ '(parrot-start-animation t)
+ '(spaceline-all-the-icons-eyebrowse-display-name nil)
+ '(spaceline-all-the-icons-hide-long-buffer-path t)
+ '(spaceline-all-the-icons-icon-set-flycheck-slim 'solid)
+ '(spaceline-all-the-icons-icon-set-git-ahead 'commit)
+ '(spaceline-all-the-icons-icon-set-git-stats 'diff-icons)
+ '(spaceline-all-the-icons-icon-set-modified 'toggle)
+ '(spaceline-all-the-icons-icon-set-sun-time 'sun/moon)
+ '(spaceline-all-the-icons-icon-set-vc-icon-git 'github-logo)
+ '(spaceline-all-the-icons-icon-set-window-numbering 'square)
+ '(spaceline-all-the-icons-separator-type 'cup)
+ '(spaceline-all-the-icons-slim-render t)
+ '(spaceline-all-the-icons-window-number-always-visible nil)
+ '(spaceline-responsive nil t)
+ '(spaceline-show-default-input-method t)
  '(truncate-lines t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
