eb66399043c9b990b99e97363141b8f9b0ddf631
zhaoxufeng1 <zhaoxufeng3@jd.com>
2019-05-24 17:28:38 +0800

    abandon Ctrl h for help(use spc h instead)

--- config/spacemacs.d/init.el
+++ config/spacemacs.d/init.el
@@ -383,6 +383,7 @@ you should place your code here."
       (ibuffer-list-buffers)
       (other-window 1)
       )
+    ;; C-q C-backspace to insert the ^? (not actually question mark)
     (define-key key-translation-map (kbd "C-h") "")
     (define-key evil-normal-state-map (kbd "C-f") 'helm-projectile-find-file-in-known-projects)
     (define-key evil-normal-state-map (kbd "C-b") 'lazy-helm/helm-mini)
