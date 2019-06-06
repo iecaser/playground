9c8eebce77fcf486b974d1b63982d9ec74590018
zxf <iecaser@163.com>
2019-06-06 19:39:39 +0800

    start ftp

--- config/config.sh
+++ config/config.sh
@@ -3,3 +3,4 @@ xcape -e 'Mode_switch=Escape'
 nohup ~/cerebro/cerebro-0.3.1-x86_64.AppImage > /dev/null 2>&1 &
 nohup guake > /dev/null 2>&1 &
 sudo sslocal -c /etc/shadowsocks/config.json -d start
+sudo service vsftpd start
