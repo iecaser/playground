8c09773375686b75f1be579932a1a6a4287e5209
Stephen.Z <iecaser@163.com>
2019-10-16 19:20:22 +0800

    doom-one theme

--- config/spacemacs.d/init.el
+++ config/spacemacs.d/init.el
@@ -68,8 +68,6 @@ values."
             )
      (colors :variables
              colors-enable-nyan-cat-progress-bar t)
-     (chinese :variables chinese-default-input-method 'pinyin
-              chinese-enable-youdao-dict t)
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
@@ -157,7 +155,7 @@ values."
    ;; List of themes, the first of the list is loaded when spacemacs starts.
    ;; Press <SPC> T n to cycle to the next theme in the list (works great
    ;; with 2 themes variants, one dark and one light)
-   dotspacemacs-themes '(
+   dotspacemacs-themes '(doom-one
                          ample
                          doom-vibrant
                          doom-tomorrow-night
