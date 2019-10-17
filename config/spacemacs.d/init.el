0003492fbbafc859dffed4541325e0e11b20535a
zxf <iecaser@163.com>
2019-10-17 21:07:24 +0800

    larger font size

--- config/spacemacs.d/init.el
+++ config/spacemacs.d/init.el
@@ -180,8 +180,9 @@ values."
    dotspacemacs-colorize-cursor-according-to-state t
    ;; Default font, or prioritized list of fonts. `powerline-scale' allows to
    ;; quickly tweak the mode-line size to make separators look not too crappy.
+
    dotspacemacs-default-font '("DejaVu Sans Mono"
-                               :size 19
+                               :size 24
                                :weight normal
                                :width normal
                                :powerline-scale 1)
