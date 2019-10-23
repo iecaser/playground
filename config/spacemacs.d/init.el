4b1860b5beaa6d97be3d6cbffe7f7c15fbb4a5a4
Stephen.Zhao <zhaoxufeng3@jd.com>
2019-10-23 10:05:25 +0800

    org-capture: rm chrome

--- config/spacemacs.d/init.el
+++ config/spacemacs.d/init.el
@@ -35,6 +35,7 @@ values."
      better-defaults
      bibtex
      docker
+     dap
      emacs-lisp
      emoji
      git
@@ -440,21 +441,6 @@ you should place your code here."
     (spacemacs/set-leader-keys-for-major-mode 'org-agenda-mode
       "." 'spacemacs/org-agenda-transient-state/body)
     )
-  (defun retrieve-chrome-current-tab-url()
-    "Get the URL of the active tab of the first window"
-    (interactive)
-    (let ((result (do-applescript
-                   (concat
-                    "set frontmostApplication to path to frontmost application\n"
-                    "tell application \"Google Chrome\"\n"
-                    "	set theUrl to get URL of active tab of first window\n"
-                    "	set theResult to (get theUrl) \n"
-                    "end tell\n"
-                    "activate application (frontmostApplication as text)\n"
-                    "set links to {}\n"
-                    "copy theResult to the end of links\n"
-                    "return links as string\n"))))
-      (format "%s" (s-chop-suffix "\"" (s-chop-prefix "\"" result)))))
   (defun get-year-and-month ()
     (list (format-time-string "%Y - Year") (format-time-string "%m - Month")))
   (defun find-month-tree ()
